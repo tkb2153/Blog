d82ecbf4bc2736e7d9d6fe46ae2e479159098823
nick <nick@funpodium.net>
2018-08-31 11:30:40 +0800

    add fake data for test

--- Http/Controllers/BlogController.php
+++ Http/Controllers/BlogController.php
@@ -23,143 +23,22 @@ class BlogController extends Controller
      */
     public function index()
     {
-
-        // $full_path_dest = resource_path('views/modules/blog2/').'csvfile2.zip';
-        // echo system("file -i -b ".$full_path_dest);
-
-        // $full_path_dest = resource_path('views/modules/blog').'';
-        // if (!File::exists(dirname($full_path_dest))) {
-        //     File::makeDirectory(dirname($full_path_dest), null, true);
-        // }
-
-        // $full_path_source = resource_path('views/modules/blog2').'';
-        // File::move($full_path_dest, $full_path_source);
-
-        // ==============
-
-        // $data = [
-        //     [
-        //         'name' => 'Microservices-A',
-        //         'endpoint' => 'https://github.com/w19900227/unitest-php',
-        //         'version' => '1.0',
-        //     ],
-        //     [
-        //         'name' => 'Microservices-B',
-        //         'endpoint' => 'https://github.com/w19900227/unitest-php',
-        //         'version' => '2.0',
-        //     ],
-        // ];
-
-        // $ms = new Register();
-        // // // $data = $ms->convertArrayToJson($data);
-        // // $d = $ms->isJson($data);
-        // // // $d = $ms->isJson($json);
-        // // dd($d);
-        // $ms->init($data);
-        // ==============
-
-        // $uri = 'https://github.com/laravel/laravel/archive/master.zip';
-        // $client = new APIClient();
-        // $response = $client->request('GET', $uri);
-
-        // $install = new Install();
-
-        // $source = storage_path('app/test.zip');
-        // $purpose = storage_path('app/aa');
-
-        // $install->download($source, $response->getBody());
-        // $install->unzip($source, $purpose);
-        // ==============
-        $class_data = [
-            'Microservices-A' => [
-                'member' => [
-                    'create' => 'Role-A',
-                    'update' => 'Role-B',
-                    'delete' => 'Role-C',
-                ],
-                'deposit' => [
-                    'create' => 'Role-A',
-                    'update' => 'Role-B',
-                    'delete' => 'Role-C',
-                ],
+        $data = [
+            [
+                'name' => 'name-1',
+                'content' => 'content-1',
             ],
-            'Microservices-B' => [
-                'task' => [
-                    'create' => 'Role-A',
-                    'update' => 'Role-B',
-                ],
+            [
+                'name' => 'name-2',
+                'content' => 'content-2',
+            ],
+            [
+                'name' => 'name-3',
+                'content' => 'content-3',
             ],
         ];
-
-        $parallel_data = [
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'member',
-                'sub_menu' => 'create',
-                'permission' => 'Role-A',
-            ],
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'member',
-                'sub_menu' => 'update',
-                'permission' => 'Role-B',
-            ],
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'member',
-                'sub_menu' => 'delete',
-                'permission' => 'Role-C',
-
-            ],
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'deposit',
-                'sub_menu' => 'create',
-                'permission' => 'Role-A',
-            ],
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'deposit',
-                'sub_menu' => 'update',
-                'permission' => 'Role-B',
-            ],
-            [
-                'name' => 'Microservices-A',
-                'menu' => 'deposit',
-                'sub_menu' => 'delete',
-                'permission' => 'Role-C',
-
-            ],
-            [
-                'name' => 'Microservices-B',
-                'menu' => 'task',
-                'sub_menu' => 'create',
-                'permission' => 'Role-A',
-
-            ],
-            [
-                'name' => 'Microservices-B',
-                'menu' => 'task',
-                'sub_menu' => 'update',
-                'permission' => 'Role-B',
-            ],
-        ];
-
-
-
-        $class = new \Libraries\Microservices\Menu\ClassDataFormat($class_data);
-        $menu = new \Libraries\Microservices\Menu\Menu($class);
-        $result = $menu->processMenus();
-        $menus = $menu->getVerifyUserRoleMenus('Role-A');
-        // dd($menus);
-        $parallel = new \Libraries\Microservices\Menu\ParallelDataFormat($parallel_data);
-        $menu = new \Libraries\Microservices\Menu\Menu($parallel);
-        $result = $menu->processMenus($parallel);
-        $menus = $menu->getVerifyUserRoleMenus('Role-A');
-        dd($menus);
-
-        dd($result);
-        // return view('blog::index');
+        
+        return view('blog::index', $data);
     }
 
     /**
--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -1,9 +1,25 @@
-@extends('blog::layouts.master')
-
 @section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('blog.name') !!}
-    </p>
+<div class="container">
+	@foreach ($data as $_ => $value)
+	<div class="row">
+		<div class="col">
+			{{$value['name']}}
+		</div>
+		<div class="col">
+			{{$value['content']}}
+		</div>
+		<div class="col">
+			@if ($menu_permission->isPermission('Microservices-A.member.create'))
+			<a href="javascript:;">create</a>
+			@endif
+			@if ($menu_permission->isPermission('Microservices-A.member.update'))
+			<a href="javascript:;">update</a>
+			@endif
+			@if ($menu_permission->isPermission('Microservices-A.member.delete'))
+			<a href="javascript:;">delete</a>
+			@endif
+		</div>
+	</div>
+	@endforeach
+</div>
 @stop
